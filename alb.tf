eb9a5f92184479034449a24527ce31611352d834
Shubham1996-aws <69294193+Shubham1996-aws@users.noreply.github.com>
2022-10-09 01:59:20 +0530

    Create alb.tf

---
+++ alb.tf
@@ -0,0 +1,65 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_id" "subnet" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+data "aws_security_group" "mysg" {
+    vpc_id      = data.aws_vpc.default.id
+}
+# target group
+resource "aws_lb_target_group" "albtg" {
+  health_check {
+      interval = 10
+      path = "/"
+      protocol = "HTTP"
+      timeout = 5
+      healthy_threshold  = 3
+      unhealthy_threshold = 2
+    }
+
+  name     = "alb-tg"
+  port     = 80
+  protocol = "HTTP"
+  target_type = "instance"
+  vpc_id   = data.aws_vpc.default.id
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_lb" "alb" {
+  name               = "ALB"
+  load_balancer_type = "application"
+  vpc_id             = [data.aws_vpc.default.id]
+  security_groups    = data.aws_security_group.mysg.id
+  subnets            = data.aws_subnets.all.id
+  tags = {
+    Name = "application-load_balancer"
+  }
+
+  resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.front_end.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.albtg.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "alb-attcah" {
+  count = length(aws_instance.web)
+  target_group_arn = aws_lb_target_group.albtg.arn
+  target_id        = aws_instance.web[count.index].id
+}
+}
+
+#alb dns
+output "alb_dns" {
+    value = aws_alb.alb.dns_name
+}
